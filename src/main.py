690a96322a2274fc180ddb81dfb61fc82a799ef8
Lucas Sato <lucasjks@gmail.com>
2021-11-27 16:57:32 -0800

    Improve training function for neural networks
    Training function now keeps a record of the training history.
    
    Training function now periodically saves the training history, as well
    as a snapshot of the model being trained.
    
    Preprocessing of the data is now done across two separate functions: one
    which is primarily performing the train-valid-test split, and one which
    is local to one of the splits.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,64 @@
 import utils
 
+import torch
 import torch.nn as nn
 
-def test_main():
-    model = utils.get_fc_nn([105, 600, 600, 1], nn.ReLU)
-    model = utils.load_nn(model, 'saved_models/test.torch')
-    return model
+TRAIN_PATH = '../data/application_data_train.csv'
+VALID_PATH = '../data/application_data_valid.csv'
+TEST_PATH = '../data/application_data_test.csv'
+MINI_PATH = '../data/application_data_mini.csv'
+
+NN_LAYERS = [244, 900, 1]
+NONLINEARITY = nn.Sigmoid
+NN_NAME = 'mini_test'
+
+def curriculum_train(model, train_x, train_y, valid_x, valid_y, batch_size,
+                     criterion, lr, momentum, num_epochs):
+    '''
+    Repeatedly train model with different training sets
+    In this first version, we first overfit the model to a small balanced 50:50
+    pos:neg ratio, then the true ratio (about 92:8)
+    '''
+    small_neg_train_x = train_x[train_y == 0][-300:]
+    small_pos_train_x = train_x[train_y == 1][-300:]
+    small_neg_train_y = train_y[train_y == 0][-300:]
+    small_pos_train_y = train_y[train_y == 1][-300:]
+    first_train_x = torch.cat((small_neg_train_x, small_pos_train_x), axis=0)
+    first_train_y = torch.cat((small_neg_train_y, small_pos_train_y), axis=0)
+    shuffle_ix = torch.randperm(600)
+    first_train_x = first_train_x[shuffle_ix]
+    first_train_y = first_train_y[shuffle_ix]
+    utils.train_nn(model, first_train_x, first_train_y, valid_x, valid_y,
+                   batch_size, criterion, lr, momentum, num_epochs // 3)
+    utils.eval_nn(model, first_train_x, first_train_y, valid_x, valid_y, criterion)
+
+    second_train_x = train_x[:600]
+    second_train_y = train_y[:600]
+    utils.train_nn(model, second_train_x, second_train_y, valid_x, valid_y,
+                   batch_size, criterion, lr, momentum, 2 * num_epochs // 3)
+    utils.eval_nn(model, second_train_x, second_train_y, valid_x, valid_y, criterion)
 
 
 def main():
-    (train, valid, _) = utils.get_data()
-    (train_X, train_y) = train
-    (valid_X, valid_y) = valid
+    (train_X, train_y) = utils.get_data_alt(MINI_PATH)
+    (valid_X, valid_y) = utils.get_data_alt(VALID_PATH)
+    print('TRAIN SHAPES')
+    print(train_X.shape)
+    print(train_y.shape)
+    print('VALIDATION SHAPES')
+    print(valid_X.shape)
+    print(valid_y.shape)
+    print(f'Training networkd with {NN_LAYERS} layers, {NONLINEARITY} nonlinearity, {NN_NAME} name.')
 
-    model = utils.get_fc_nn([105, 900, 900, 900, 900, 1], nn.Sigmoid)
+    model = utils.get_fc_nn(NN_LAYERS, NONLINEARITY)
 
     batch_size = 1
     criterion = nn.BCELoss()
     lr = 0.001
     momentum = 0.9
-    num_epochs = 100
-    utils.train_nn(model, train_X[:100], train_y[:100], valid_X, valid_y, batch_size, criterion, lr, momentum, num_epochs)
+    num_epochs = 15
+    model, training_history = utils.train_nn(model, train_X, train_y, valid_X, valid_y,
+            batch_size, criterion, lr, momentum, num_epochs, NN_NAME)
 
 if __name__ == '__main__':
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,41 +1,99 @@
+import json
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 
-torch.manual_seed(27)
+ALL_DATA_PATH = '../data/application_data.csv'
 
-def get_data():
-    '''
-    Read and preprocess the dataset.
+torch.manual_seed(0)
 
-    Returns:
-    3-tuple containing 3 2-tuples: (train_x, train_y), (val_x, val_y), (test_x, 
-    test_y), with a 70:15:15 ratio.
-    '''
-    # TODO: We can probably do it cheaper without dataframes? This is currently 
-    # copied straight out of Matheus's Collab notebook.
-    # TODO: Parametrize file path
-    app_df = pd.read_csv('../data/application_data.csv')
+def train_valid_test_split():
+    np.random.seed(0)
+    df = pd.read_csv(ALL_DATA_PATH)
     # One hot encode
-    to_encode = ['NAME_CONTRACT_TYPE', 'CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY', 
-               'NAME_TYPE_SUITE', 'NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS', 
+    to_encode = ['NAME_CONTRACT_TYPE', 'CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY',
+               'NAME_TYPE_SUITE', 'NAME_INCOME_TYPE', 'NAME_EDUCATION_TYPE', 'NAME_FAMILY_STATUS',
                'NAME_HOUSING_TYPE', 'OCCUPATION_TYPE', 'WEEKDAY_APPR_PROCESS_START', 'ORGANIZATION_TYPE',
                'FONDKAPREMONT_MODE', 'HOUSETYPE_MODE', 'WALLSMATERIAL_MODE', 'EMERGENCYSTATE_MODE' ]
 
-    encoded = pd.get_dummies(to_encode) # this is not correct, check
-    app_df.drop(to_encode, axis = 1, inplace  = True)
-    app_df.merge(encoded, left_index= True, right_index = True)
+    encoded = pd.get_dummies(df[to_encode])
+    df = df.drop(to_encode, axis=1)
+    df = df.merge(encoded, left_index=True, right_index=True)
+
+    df = df.sample(frac=1, random_state=0, ignore_index=True)
+    train, valid, test = np.split(df,
+               [int(.7*len(df)), int(.85*len(df))])
+    train = train.reset_index(drop=True)
+    valid = valid.reset_index(drop=True)
+    test = test.reset_index(drop=True)
+    mini = train.sample(frac=0.005, random_state=0, ignore_index=True)
+
+    train.to_csv('../data/application_data_train.csv', index=False)
+    valid.to_csv('../data/application_data_valid.csv', index=False)
+    test.to_csv('../data/application_data_test.csv', index=False)
+    mini.to_csv('../data/application_data_mini.csv', index=False)
+
+def get_data_alt(path):
+    '''
+    Load and preprocess the dataset.
+
+    Arguments:
+    -path: the file path for the dataset to be loaded and preprocessed.
+
+    Returns:
+    2-tuple containing the X values and the y values of the dataset in the
+    given path
+    '''
+    # TODO: We can probably do it cheaper without dataframes? This is currently
+    # copied straight out of Matheus's Collab notebook.
+    app_df = pd.read_csv(path)
 
     # Fill NaN
 
     app_df = app_df.fillna(app_df.mean())
 
-    # Separate X and Y into arrays 
+    # Drop ID
+
+    app_df = app_df.drop('SK_ID_CURR', axis = 1)
+
+    X = app_df.drop('TARGET', axis = 1).values.astype('float32')
+    y = app_df.TARGET.values.astype('float32')
+
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
+
+    X = torch.from_numpy(X)
+    y = torch.from_numpy(y)
+
+    return X, y
+
+def get_data(path):
+    '''
+    Load and preprocess the dataset.
+
+    Arguments:
+    -path: the file path for the dataset to be loaded and preprocessed.
+
+    Returns:
+    2-tuple containing the X values and the y values of the dataset in the
+    given path
+    '''
+    # TODO: We can probably do it cheaper without dataframes? This is currently
+    # copied straight out of Matheus's Collab notebook.
+    app_df = pd.read_csv(path)
+
+    # Fill NaN
+
+    app_df = app_df.fillna(app_df.mean())
+
+    # Separate X and Y into arrays
 
     X = app_df.drop('TARGET', axis = 1).values.astype('float32')
     y = app_df.TARGET.values.astype('float32')
@@ -47,40 +105,25 @@ def get_data():
     X = torch.from_numpy(X)
     y = torch.from_numpy(y)
 
-    # Train-val-test split
-    num_examples = X.size()[0]
-    shuffle_ix = torch.randperm(num_examples)
-    first_valid_ix = int(num_examples * 0.7)
-    last_valid_ix = int(num_examples * 0.85)
-    train_X = X[shuffle_ix][:first_valid_ix]
-    train_y = y[shuffle_ix][:first_valid_ix]
-
-    valid_X = X[shuffle_ix][first_valid_ix:last_valid_ix]
-    valid_y = y[shuffle_ix][first_valid_ix:last_valid_ix]
-
-    test_X = X[shuffle_ix][last_valid_ix:]
-    test_y = y[shuffle_ix][last_valid_ix:]
-    return ((train_X, train_y), (valid_X, valid_y), (test_X, test_y))
+    return X, y
 
 def get_fc_nn(layer_sizes, activation):
     '''
     Instantiate a fully connected feedforward neural network.
 
     Parameters:
-
-        *layer_sizes is an array of positive integers representing the number of 
-    units in each layer. Must be of length at least two, since layer_sizes[0] 
-    is taken to be the number of input variables and layer_sizes[-1] is the 
+        *layer_sizes is an array of positive integers representing the number of
+    units in each layer. Must be of length at least two, since layer_sizes[0]
+    is taken to be the number of input variables and layer_sizes[-1] is the
     number of output variables.
-        *activation is an activation (e.g. nn.ReLU) to be applied to each 
+        *activation is an activation (e.g. nn.ReLU) to be applied to each
     hidden layer of the model.
-        *loss_function is a function to be applied to the last layer, and the 
-    corresponding value(s) of train_y in order to calculate the loss of the 
+        *loss_function is a function to be applied to the last layer, and the
+    corresponding value(s) of train_y in order to calculate the loss of the
     model.
 
     Returns:
-    A 2-tuple containing a new instance of a fully connected feedforward neural 
-    network.
+    A new instance of a fully connected feedforward neural network.
     '''
     layers = []
     num_layers = len(layers)
@@ -93,27 +136,28 @@ def get_fc_nn(layer_sizes, activation):
             layers.append(nn.Sigmoid())
     return nn.Sequential(*layers)
 
-def train_nn(model, train_x, train_y, valid_x, valid_y, batch_size, criterion, 
-             lr, momentum, num_epochs):
+def train_nn(model, train_x, train_y, valid_x, valid_y, batch_size, criterion,
+             lr, momentum, num_epochs, model_name):
     '''
     Train a neural network using Stochastic Gradient Descent.
 
     Parameters:
-
         *model is an instance of an nn.Module subclass.
         *train_x is a tensor representing the training data's attributes.
         *train_y is a tensor representing the training data's labels.
         *valid_x is a tensor representing the validation data's attributes.
         *valid_y is a tensor representing the validation data's labels.
         *batch_size is the size of the minibatch.
-        *criterion is a function to be applied to the model output and the 
+        *criterion is a function to be applied to the model output and the
     train labels in order to calculate the loss of the model.
         *lr is a float representing the learning rate.
         *lr is a float representing the training momentum.
         *num_epochs is an integer representing the number of epochs.
+        *model_name is a string. The resulting model will be saved as
+        f'saved_models/{model_name}.torch'
 
     Returns:
-    A 2-tuple containing a trained instance of an nn.Module subclass, and the 
+    A 2-tuple containing a trained instance of an nn.Module subclass, and the
     training history
     '''
     shuffle_ix = torch.randperm(train_x.size()[0])
@@ -121,8 +165,14 @@ def train_nn(model, train_x, train_y, valid_x, valid_y, batch_size, criterion,
     train_y = train_y[shuffle_ix]
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
     len_train_x = len(train_x)
+    training_history = {
+        'epoch': [],
+        'train_acc': [],
+        'train_loss': [],
+        'valid_acc': [],
+        'valid_loss': [],
+    }
     for epoch in range(num_epochs):
-        running_loss = 0.0
         i = 0
         batch_start = i * batch_size
         while batch_start < len_train_x:
@@ -134,24 +184,39 @@ def train_nn(model, train_x, train_y, valid_x, valid_y, batch_size, criterion,
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
-            # print statistics
-            running_loss += loss.item()
-            if i == 0:
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss))
-                running_loss = 0.0
-            if i == 0 and epoch == 0:
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss))
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
             i += 1
             batch_start += batch_size
-    torch.save(model.state_dict(), 'saved_models/mini.torch')
-    return model
+
+        # print statistics
+        train_loss, train_acc, valid_loss, valid_acc = eval_nn(model,
+                train_x, train_y, valid_x, valid_y, criterion)
+        print('[epoch %d]\ttrain L: %.3f\tvalid L: %.3f\ttrain A: %.3f\tvalid A: %.3f' %
+              (epoch + 1, train_loss, valid_loss, train_acc, valid_acc))
+        training_history['epoch'].append(epoch)
+        training_history['train_acc'].append(train_acc.item())
+        training_history['train_loss'].append(train_loss.item())
+        training_history['valid_loss'].append(valid_loss.item())
+        training_history['valid_acc'].append(valid_acc.item())
+
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(), f'saved_models/{model_name}_{epoch}.torch')
+            with open(f'saved_models/{model_name}_training_history.json', 'w') as f:
+                f.write(json.dumps(training_history))
+    torch.save(model.state_dict(), f'saved_models/{model_name}_{num_epochs}.torch')
+    with open(f'saved_models/{model_name}_training_history.json', 'w') as f:
+        f.write(json.dumps(training_history))
+    return model, training_history
+
+def eval_nn(model, train_x, train_y, test_x, test_y, criterion):
+    '''
+    Evaluate a given model according to accuracy and the training criterion,
+    for both train set and a test set.
+    '''
+    train_loss = criterion(model(train_x), train_y.unsqueeze(1))
+    train_acc = sum(torch.round(model(train_x)) == train_y.unsqueeze(1)) / train_x.shape[0]
+    test_loss = criterion(model(test_x), test_y.unsqueeze(1))
+    test_acc = sum(torch.round(model(test_x)) == test_y.unsqueeze(1)) / test_x.shape[0]
+    return (train_loss, train_acc, test_loss, test_acc)
 
 def load_nn(model, path):
     model.load_state_dict(torch.load(path))
